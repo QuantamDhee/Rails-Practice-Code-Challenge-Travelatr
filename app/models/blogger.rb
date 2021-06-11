50c56637d601299787effdd320e961dcc24d61a6
Johndhee <Johndheeballesteros13@gmail.com>
2021-06-11 15:08:52 -0700

    not done

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,13 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 
+    validates :name, {uniqueness: true, presence: true}
+    validates :age, numericality: {greater_than: 0}
+    validates :bio, length: {minimum: 30}
+
+  def featured_post
+    post = self.posts.max{|a, b| a.likes <=> b.likes}
+    post.title
+  end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,9 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
+
+    def get_last
+        self.posts.last(5)
+    end
 
 end
